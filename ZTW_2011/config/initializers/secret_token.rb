f500407c9e01e2aa8bcabf616e38fa03c2fdc8ba
Marcin Kosinski <kosinski.marcin@gmail.com>
2011-03-06 23:43:12 +0100

    first commit from netbeans

---
+++ ZTW_2011/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZTW2011::Application.config.secret_token = '62347f87c5ebc169660758ab8cced20f6ce5a89150c5c60bc618107a0710a5317ecd82e90d6eaf9b1c7a2d3ede04dc8912f02cc761eb12e9209dc773f26dcf0e'
